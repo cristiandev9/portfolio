cbfc906b30edd28742d381fdd5d5df1b456d94eb
Cristian <cristian.andrisen@gmail.com>
2022-03-02 19:19:56 -0300

    first

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const { resolve } = require('path')
+
+const app = express()
+
+
+app.use('/', express.static(
+  resolve(
+    __dirname,
+    './build'
+  )
+))
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+  if (err) {
+    return console.log(err)
+  }
+
+  console.log('Tudo funcionando')
+})
